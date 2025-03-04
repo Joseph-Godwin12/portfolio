08eaf778ef6995b1036be51676367bb7a5689d31
Joseph-Godwin12 <josephgodwin768@gmail.com>
2025-03-04 22:49:31 +0100

    Added images and changed the title

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,7 +1,17 @@
-export default function Project() {
-    return (
-        <div className="flex flex-col items-center justify-center h-screen">
-        
+ export default function Project() {
+     return (
+        <div>
+            hello
         </div>
+//         <div className="flex flex-col items-center justify-center h-screen">
+//             <h1 className="my-20 text-center text-4xl">Projects</h1>
+//             <div>
+//                 {PROJECTS.map((project, index) => (
+//                     <div key={index} className="flex flex-wrap items-center justify-center p-4">
+//                         <img src={project.img} alt={project.title} className="w-1/2 h-1/2" />
+//                     </div>
+//                 ))}
+//             </div>
+//         </div>
     )
-}
+ }
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,8 @@
+import colorGuess from "../assets/color-g.png"
+import textProcessor from "../assets/text-p.png"
+import ticketGenerator from "../assets/ticket-g.png"
+import todoList from "../assets/Todo-list.png"
+import quotes from "../assets/quotes.png"
 
 export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With 5 years of hands-on experience, I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, MySQL, PostgreSQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
 
@@ -12,50 +17,34 @@ export const EXPERIENCES = [
     description: `Developed user interfaces for web applications using React. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
     technologies: ["Javascript", "React.js", "TailwindCss"],
   },
-  // {
-  //   year: "2022 - 2023",
-  //   role: "Frontend Developer",
-  //   company: "Adobe",
-  //   description: `Designed and developed user interfaces for web applications using Next.js and React. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
-  //   technologies: ["HTML", "CSS", "Vue.js", "mySQL"],
-  // },
-  // {
-  //   year: "2021 - 2022",
-  //   role: "Full Stack Developer",
-  //   company: "Facebook",
-  //   description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
-  //   technologies: ["Python", "Svelte", "Three.js", "Postgres"],
-  // },
-  // {
-  //   year: "2020 - 2021",
-  //   role: "Software Engineer",
-  //   company: "Paypal",
-  //   description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
-  //   technologies: ["Ruby", "Rails", "PHP", "Sqlite"],
-  // },
+  
 ];
 
 export const PROJECTS = [
   {
-    title: "E-Commerce Website",
+    img: colorGuess,
+    title: "Color Guessing Game",
     description:
       "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
     technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
   },
   {
-    title: "Task Management App",
+    img: textProcessor,
+    title: "Text Processor",
     description:
       "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
     technologies: ["HTML", "CSS", "Angular", "Firebase"],
   },
   {
-    title: "Portfolio Website",
+    img: ticketGenerator,
+    title: "Ticket Generator",
     description:
       "A personal portfolio website showcasing projects, skills, and contact information.",
     technologies: ["HTML", "CSS", "React", "Bootstrap"],
   },
   {
-    title: "Blogging Platform",
+    img: todoList,
+    title: "Todo List App",
     description:
       "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
     technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
